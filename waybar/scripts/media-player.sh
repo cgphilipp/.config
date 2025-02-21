c3edc5b3c55878a6d8054f811f0ca01daf060ee2
Philipp Otto <philipp-otto@posteo.de>
2025-02-21 17:20:42 +0100

    Use color variables for consistent look

--- waybar/scripts/media-player.sh
+++ waybar/scripts/media-player.sh
@@ -1,10 +1,14 @@
-#!/bin/sh
+#!/usr/bin/bash
 
 player_status=$(playerctl --player spotify,%any status 2> /dev/null)
+(($? != 0)) && { exit 1; }
+
 artist=$(playerctl --player spotify,%any metadata artist)
 title=$(playerctl --player spotify,%any metadata title)
 if [ "$player_status" = "Playing" ]; then
-    echo " <b>${artist}</b> - ${title}"
+    icon=""
 elif [ "$player_status" = "Paused" ]; then
-    echo " <b>${artist}</b> - ${title}"
+    icon=""
 fi
+
+echo "${icon} <b>${artist}</b> - ${title}"
